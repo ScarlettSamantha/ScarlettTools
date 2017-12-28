d315f51e6a81d3c9c9f9dc04210444b3144aceaf
Scarlett Verheul <scarlett@arcane-tech.nl>
2017-12-28 11:44:10 +0100

    [TASK] Make the routing process better.
    Prevent dual initalisation of a class object.
    Also added apache support to the ctn module

--- __main__.py
+++ __main__.py
@@ -4,20 +4,30 @@ import inspect
 
 from importlib import import_module
 
+MAGIC_PROPERTY_METHOD = 'METHOD'
+
 if __name__ == "__main__":
     if sys.argv.__len__() <= 1:
         print("Build more parameters")
         exit(1)
 
-    args_ = ([] if sys.argv.__len__() <= 3 else sys.argv[3:])
-    class_ = getattr(import_module("commands.%s" % sys.argv[1].lower()), sys.argv[1].lower().title())(*args_)
-    method_ = getattr(class_, sys.argv[2])
+    class_ = getattr(import_module("commands.%s" % sys.argv[1].lower()), sys.argv[1].lower().title())(*sys.argv[2:])
+    if hasattr(class_, MAGIC_PROPERTY_METHOD):
+        method_ = getattr(class_, getattr(class_, MAGIC_PROPERTY_METHOD))
+        args_ = ([] if sys.argv.__len__() <= 3 else sys.argv[2:])
+        send_args = False
+    else:
+        args_ = ([] if sys.argv.__len__() <= 3 else sys.argv[3:])
+        method_ = getattr(class_, sys.argv[2])
+    send_args = True
 
     if inspect.isgeneratorfunction(method_):
         for i in method_(*args_):
             print(i)
     else:
-        r = method_(*args_)
+        if send_args:
+            r = method_(*args_)
+        else:
+            r = method_()
         if r is not None:
             print(r)
-
--- commands/ctn.py
+++ commands/ctn.py
@@ -5,14 +5,13 @@ import html
 class Ctn:
 
     DEFAULT_FILE_LOCATION = '/tmp/conversion_output.txt'
+    DEFAULT_DELIMITER = ';'
 
-    @classmethod
-    def convert(cls, i_file: str, output: str=None, delimiter: str=';', decode: bool=True):
-        c = cls(i_file, output, delimiter, decode)
-        c.parse_file()
-        c.to_file()
+    MODE_APACHE = 0
+    MODE_NGINX = 1
+    METHOD = "do"
 
-    def __init__(self, file: str, output: str=None, delimiter: str=';', decode: bool=True):
+    def __init__(self, file: str, output: str=None, mode: int=MODE_NGINX, delimiter: str=DEFAULT_DELIMITER, decode: bool=True) -> None:
         self.output = []
         self.file = os.path.abspath(file)
         if os.path.exists(self.file) is False:
@@ -23,9 +22,16 @@ class Ctn:
         self.delimiter = delimiter
         self.out_f = self.DEFAULT_FILE_LOCATION if output is None else os.path.abspath(output)
         self.in_fp = open(file, mode='r')
+        self.mode = mode
+
+    def do(self):
+        self.parse_file()
+        self.to_file()
 
     def parse_file(self) -> None:
-        print('Taking input from %s and outputing to %s with delimiter %s' % (self.file, self.out_f, self.delimiter))
+        str_args = (self.file, self.out_f, self.delimiter, 'Apache' if self.mode is self.MODE_APACHE else 'Nginx')
+        print('Taking input from %s and outputting to %s with delimiter %s in %s mode' % str_args)
+        del str_args
         for i, line in enumerate(self.in_fp):
             if i == 0:
                 continue
@@ -35,13 +41,16 @@ class Ctn:
             self.convert_line(source=source, destination=destination, permanent=True)
 
     def convert_line(self, source, destination, permanent=True) -> None:
-        self.output.append('rewrite ^%s$ %s%s' % (source, destination.rstrip(), ' permanent;' if permanent else ';'))
+        if self.mode == self.MODE_NGINX:
+            self.output.append('rewrite ^%s$ %s%s' % (source, destination.rstrip(), ' permanent;' if permanent else ';'))
+        elif self.mode == self.MODE_APACHE:
+            self.output.append('RewriteRule "^%s$" "%s"%s' % (source, destination.rstrip(), ' [R]' if permanent else ''))
 
     def to_file(self) -> None:
         f = open(self.out_f, 'w+')
         f.truncate(0)
         f.seek(0)
-        f.write('\r\n'.join(self.output))
+        f.write(os.linesep.join(self.output))
         f.flush()
         f.close()
 
